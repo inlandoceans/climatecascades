a6f16704468fc1370116dc2c5e705ea4611d4fd0
inlandoceans <realeh@outlook.com>
2020-10-22 16:18:23 +0200

    small updates

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,7 @@
     <img src="/tempbars.png" />
     <p class="text-sm uppercase">Average temperature anomalies, by NOAA</p>
     <h2 class="mt-8 text-2xl">Climate Cascades</h2>
-    <p class="mt-4">
+    <p class="mt-4 text-lg">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
       viverra at diam sed vehicula. Donec et eros et neque faucibus
       pellentesque. Nunc molestie gravida augue in tristique. Suspendisse
@@ -31,28 +31,6 @@
       Duis gravida felis sed lacus efficitur, quis faucibus odio auctor. Nulla
       mauris ante, dapibus in posuere vel, rhoncus eget orci.
     </p>
-    <p>
-      Aliquam a bibendum augue. Phasellus urna nulla, mollis non fringilla sit
-      amet, pharetra vitae dui. Sed consequat pellentesque laoreet. Donec
-      ultricies volutpat mauris ac tempus. Sed in orci ligula. Pellentesque sem
-      sem, euismod ut lorem bibendum, auctor accumsan dolor. Pellentesque mattis
-      quis magna vel molestie. Donec orci augue, lobortis at magna eu, molestie
-      sodales neque. Vestibulum vel purus est. Aliquam erat volutpat. Donec
-      tincidunt massa arcu, a porta metus pulvinar eget. Proin blandit blandit
-      sem, vitae mollis nibh lacinia non. Donec at sem eget mauris laoreet
-      sagittis eu sit amet ex. Donec rhoncus ex sed ligula blandit semper. Etiam
-      feugiat ipsum enim, eget sodales nunc varius et. Nunc id consequat velit,
-      id venenatis mi.
-    </p>
-    <p>
-      Aliquam pulvinar ipsum sapien, quis elementum ex hendrerit quis. Aliquam
-      imperdiet fringilla nibh, eu ultrices arcu volutpat et. In et mollis
-      justo. Ut luctus turpis quis aliquam lacinia. Suspendisse convallis
-      consequat malesuada. Fusce eu ligula quis arcu blandit tristique a quis
-      magna. Vestibulum sit amet accumsan tellus. Curabitur sit amet justo
-      vestibulum, malesuada ipsum sit amet, lacinia ante. Aenean sit amet
-      rhoncus elit. Suspendisse ornare tincidunt felis, et porttitor eros
-      feugiat vitae.
-    </p>
   </div>
 </template>
+<script></script>
